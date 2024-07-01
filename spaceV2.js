3c9a56e1fa6cffd916239e05a8da3103ccbad316
redfoxbg <redfoxbg@gmail.com>
2024-07-01 22:43:01 +0300

    Some refactoring

--- spaceV2.js
+++ spaceV2.js
@@ -1,3 +1,21 @@
+function drawV2() {
+    background(30);
+
+    if (!singleTimeSetUp) {
+        for (let i = 0; i < 1000; i++) {
+            stars[i] = new StarV2();
+        }
+        print('Mouse Right click to return back to the menu');
+        singleTimeSetUp = true;
+    }
+
+    stars.forEach(star => {
+        star.show();
+    });
+
+    checkForBackToMenu();
+}
+
 class StarV2 {
     constructor() {
         this.x = random(width);
@@ -83,3 +101,4 @@ class StarV2 {
 
     }
 }
+
